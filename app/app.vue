2224c6fe8dd8160702de4e8584e461dfcc8c4817
Sofia Joy Yunun <yununsofiajoy1521@gmail.com>
2024-12-05 16:31:25 +0800

    update

---
+++ app/app.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="font-Montserrat text-dire-wolf">
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+  </div>
+</template>
---
+++ app/layouts/default.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+      <BaseNavigation />
+      <slot />
+    </div>
+  </template>
+  
+  <script>
+  export default {
+  
+  }
+  </script>
